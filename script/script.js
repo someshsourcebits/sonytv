5027a1e16eeed99b724ef5d57e8fa8cc2c8f3011
someshsourcebits <43070321+someshsourcebits@users.noreply.github.com>
2018-09-17 19:37:16 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,144 +1,193 @@
 $(function () {
 
 
-	$('.keyboard').keyboard({
-
-		layout: 'international',
-		// only use !! in the action key layout
+/*** MENU slider  */
 
 
 
-	}).previewKeyset({
-		sets: ['normal']
-	});
+    $('#slider1 ul li').click(function () {
+        a = $('#slider1 li').length;
+        $('#slider1 ul li').animate({
+            right: "+=200px"
+        });
 
-	/*----------home page script *-------*/
 
-
-
-	/*---------------------*/
-
-	$(function() {
-        $('.lazy').Lazy();
     });
 
 
-	//scrool homepage menu
+
+    var slideCount = $('#slider1 ul li').length;
+    var slideWidth = $('#slider1 ul li').width();
+    var slideHeight = $('#slider1 ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
+
+
+
+
+    $('#slider1').css({
+        "width": "100%",
+        "height": "200px"
+    });
+
+    $('#slider1 ul').css({
+        width: sliderUlWidth,
+        marginLeft: -slideWidth
+    });
+
+
+    $("#slider1 ul li").keyup(function (e) {
+        var $this = $(".active");
+
+        if (e.keyCode == 37) { // left
+
+
+
+
+            $('#slider1 ul li').animate({
+                right: "+=200px"
+            });
+            
+            $this.removeClass('active');
+            $this.next().addClass('active');
+
+
+
+        } else if (e.keyCode == 39) { // right
+
+            $('#slider1 ul li').animate({
+                left: "-=200px"
+            });
+
+
+            $this.prev().addClass('active');
+            $this.removeClass('active');
+        }
+    });
+
+
+
+/** menus slider ends */
+
+    /*---------------------*/
+
+
+
+    //scrool homepage menu
 
 
 
 
 
-	var slideCount = $('#slider ul li').length;
-	var slideWidth = $('#slider ul li').width();
-	var slideHeight = $('#slider ul li').height();
-	var sliderUlWidth = slideCount * slideWidth;
+    var slideCount = $('#slider ul li').length;
+    var slideWidth = $('#slider ul li').width();
+    var slideHeight = $('#slider ul li').height();
+    var sliderUlWidth = slideCount * slideWidth;
 
 
 
 
-	$('#slider').css({
-		"width": "100%"
-		, "height": "200px"
-	});
+    $('#slider').css({
+        "width": "100%",
+        "height": "200px"
+    });
 
-	$('#slider ul').css({
-		width: sliderUlWidth
-		, marginLeft: -slideWidth
-	});
+    $('#slider ul').css({
+        width: sliderUlWidth,
+        marginLeft: -slideWidth
+    });
 
-	$('#slider ul li:last-child').prependTo('#slider ul');
+    $('#slider ul li:last-child').prependTo('#slider ul');
 
-	function moveLeft() {
-		$('#slider ul').animate({
-			left: +slideWidth
-		}, 200, function () {
-			$('#slider ul li:last-child').prependTo('#slider ul');
-			$('#slider ul').css('left', '');
-			//$("#slider ul li").removeClass("active");
-
-
-		});
-	};
-
-	function moveRight() {
-		$('#slider ul').animate({
-			left: -slideWidth
-		}, 200, function () {
-			$('#slider ul li:first-child').appendTo('#slider ul');
-			$('#slider ul').css('left', '');
-
-		});
-	};
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: +slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+            //$("#slider ul li").removeClass("active");
 
 
+        });
+    };
 
-	$("body").keyup(function (e) {
-		var $this = $(".active");
+    function moveRight() {
+        $('#slider ul').animate({
+            left: -slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
 
-		if (e.keyCode == 37) { // left
+        });
+    };
 
 
 
+    $("body").keyup(function (e) {
+        var $this = $(".active");
 
-
-			moveRight();
-			$this.removeClass('active');
-			$this.next().addClass('active');
-
-
-
-		} else if (e.keyCode == 39) { // right
-
-			moveLeft();
-
-
-			$this.prev().addClass('active');
-			$this.removeClass('active');
-		}
-	});
+        if (e.keyCode == 37) { // left
 
 
 
 
 
-
-
-	// $(".navbar").scroll(function() {
-
-	// 	if ($(this).scrollTop()<0)
-	// 	 {
-
-	// 		$('.fade').fadeOut();
-
-	// 	 }
-	// 	else
-	// 	 {
-	// 	  $('.fade').fadeIn();
-	// 	 }
-	//  });
+            moveRight();
+            $this.removeClass('active');
+            $this.next().addClass('active');
 
 
 
+        } else if (e.keyCode == 39) { // right
 
-	$(window).scroll(function () {
-		var threshold = 200; // number of pixels before bottom of page that you want to start fading
-		var value = ($(window).scrollTop);
+            moveLeft();
+
+
+            $this.prev().addClass('active');
+            $this.removeClass('active');
+        }
+    });
 
 
 
 
-		var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / threshold;
 
-		if (op <= 0) {
 
-			$("#thing-to-hide").hide();
-		} else {
-			$("#thing-to-hide").show();
-		}
-		$("#thing-to-hide").css("opacity", op);
 
-	});
+    // $(".navbar").scroll(function() {
+
+    // 	if ($(this).scrollTop()<0)
+    // 	 {
+
+    // 		$('.fade').fadeOut();
+
+    // 	 }
+    // 	else
+    // 	 {
+    // 	  $('.fade').fadeIn();
+    // 	 }
+    //  });
+
+
+
+
+    $(window).scroll(function () {
+        var threshold = 200; // number of pixels before bottom of page that you want to start fading
+        var value = ($(window).scrollTop);
+
+
+
+
+        var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / threshold;
+
+        if (op <= 0) {
+
+            $("#thing-to-hide").hide();
+        } else {
+            $("#thing-to-hide").show();
+        }
+        $("#thing-to-hide").css("opacity", op);
+
+    });
 
 
 
